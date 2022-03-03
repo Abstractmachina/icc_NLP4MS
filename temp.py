4d2a4e2dc184fcc90f4285e06cbcead7fb00ae48
Ronan <rcp20@ic.ac.uk>
2022-03-03 18:05:41 +0000

    # I kept getting wierd ass errors when I tried to .pack() with the above code so I tried creating a second frame to see if a function for user input would work

--- temp.py
+++ temp.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 from tkinter import *
 from tkinter import ttk
 
@@ -34,3 +27,30 @@ ttk.Label(frm, text="Enter to search:").grid(column=0, row=0)
 ent = ttk.Entry(root)
 btn = ttk.Button(frm, text="Search", command=displayWordCount(frm, ent)).grid(column=1, row=0)
 root.mainloop()
+
+
+
+# I kept getting wierd ass errors when I tried to .pack() with the above code?
+# Created a second frame to write a function to take in/print user input
+
+second_frame= Tk()
+second_frame.geometry("1000x500")
+
+def printUserInputToScreen():
+   global entry
+   string= entry.get()
+   label.configure(text=string)
+
+def takeUserInput():
+    global entry
+    string = entry.get()
+    return string
+
+label=Label(second_frame, text="", font=("Calibri 22 bold"))
+label.pack()
+entry= Entry(second_frame, width= 40)
+entry.focus_set()
+entry.pack()
+ttk.Button(second_frame, text= "Input Text then Click Me",width= 20, command= printUserInputToScreen).pack(pady=20)
+
+second_frame.mainloop()
