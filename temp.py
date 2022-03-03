0771f227283d047891b462133f1ea8c5899bbbb1
Colin Taylor <cht21@ic.ac.uk>
2022-03-03 13:57:51 +0000

    attempted a simple search app. don't know how to get text from the user yet

--- temp.py
+++ temp.py
@@ -8,18 +8,29 @@ This is a temporary script file.
 from tkinter import *
 from tkinter import ttk
 
-data_dict = {'ID': [1, 2, 3, 4, 5], 'free_text': 
-             ['my leg has been hurting', 
-              'i can\'t sleep',
-              'a year ago it was much worse',
-              'it was difficult having covid'
-              ]}
+import pandas as pd
 
-data_frame = pd.DataFrame(data=data_dict)
+data = ['my leg has been hurting', 
+        'i can\'t sleep',
+        'a year ago it was much worse',
+        'it was difficult having covid',
+        'the last year has been difficult as i went into relapse']
+
+
+def searchData(word):
+    count = 0
+    for string in data:
+        if (string.find(word)!=-1):
+            count += 1
+    return count
+
+def displayWordCount(frm, ent):
+   ttk.Label(frm, text=searchData(ent.get())).grid(column=0, row=1)
 
 root = Tk()
 frm = ttk.Frame(root, padding=10)
 frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+ttk.Label(frm, text="Enter to search:").grid(column=0, row=0)
+ent = ttk.Entry(root)
+btn = ttk.Button(frm, text="Search", command=displayWordCount(frm, ent)).grid(column=1, row=0)
 root.mainloop()
