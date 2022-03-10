6b4ac4e12625bc3ea9efe472b4cbcf4592a7f273
Kaixuan Khoo <kaixuan.khoo.98@gmail.com>
2022-03-10 15:15:17 +0000

    added the random model compatibility, the final model would just need to be imported (and logistic regression imported from scikit)

--- predict_adapter.py
+++ predict_adapter.py
@@ -1,4 +1,6 @@
 import random
+import pickle
+from random_model import Model
 
 MS_type_labels = {
         0: 'PPMS',
@@ -8,7 +10,26 @@ MS_type_labels = {
         4: 'Unknown'
     }
 
+# model_name = 'regression_model_bow.sav'
+model_name = 'random_model.sav'
+
+class TextPredictor:
+    def __init__(self, model_name):
+        """
+        Initialization for the TextPredictor class
+        """
+        self.model = pickle.load(open(model_name, 'rb'))
+
+    def predict(self, predict_text):
+        return self.model.predict(predict_text)
+
+    
+textPredictor = TextPredictor(model_name=model_name)
+
 def predict(predict_text):
     print(predict_text)
+
     # here we should import the model, and call the actual predict funtions
-    return (random.choice(list(MS_type_labels.values())))
+    ans = textPredictor.predict(predict_text=predict_text)
+
+    return MS_type_labels[ans]
